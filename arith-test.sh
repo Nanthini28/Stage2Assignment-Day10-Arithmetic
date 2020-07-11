b43dc74cb13558b725feb46c5b2958fe933a721b
Nanthini28 <nandhuactive28@gmail.com>
2020-07-12 00:45:48 +0530

    Store the result in dictionary

--- arith-test.sh
+++ arith-test.sh
@@ -11,3 +11,15 @@ f=$(( (($c + $a)) / $b))
 echo "ResultU4:"$f
 g=$((  (($a%$b)) + $c))
 echo "ResultU5:"$g
+#Store the result in dictonary and read the values from dictonary into array
+declare -A Result
+Result[res]=$res
+Result[e]=$e
+Result[f]=$f
+Result[g]=$g
+
+
+echo "U1" ${Result[res]}
+echo "U2" ${Result[e]}
+echo "U3" ${Result[f]}
+echo "U4" ${Result[g]}
